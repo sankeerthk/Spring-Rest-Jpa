b2217524e4b6f336b59007a0922dbc295771589d
SANKI\sankeerth <sankeerth>
2017-07-24 16:46:04 -0500

    intial Commit

---
+++ src/main/java/io/jpa/config/ServletInitializer.java
@@ -0,0 +1,22 @@
+package io.jpa.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { AppConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/api/*"};
+	}
+
+}
